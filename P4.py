789d997a09343f5e6e4d0da5b7201cf6131347fe
shubhpatel2802 <shubhpatel2802@gmail.com>
2023-12-02 21:26:36 +0530

    P4.py

---
+++ P4.py
@@ -0,0 +1,11 @@
+class Father:
+    def Lands(self):
+        print("Having 50 Ekar Lands")
+
+class Son(Father):
+    def Money(self):
+        print("having 10 lakhs Money")
+
+S=Son()
+S.Lands()
+S.Money()
