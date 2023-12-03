02c7dc222606256d4a58b0b626ce0a48733f2ea8
shubhpatel2802 <shubhpatel2802@gmail.com>
2023-12-03 10:02:05 +0530

    Merge remote changes

--- P3.py
+++ P3.py
@@ -1,6 +1,51 @@
-import turtle as t
-t.bgcolor("black")
-t.begin_fill("red")
-t.pencolor("white")
-t.circle(300)
-t.end_fill()
+'''
+#Break Statement
+
+=>EX:-1
+
+fruits = ["apple", "banana", "cherry"]
+for i in fruits:
+  if i == "banana":
+    break
+  print(i)
+
+=>EX:-2
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+#Continue Statement
+
+fruits = ["apple", "banana", "cherry"]
+for i in fruits:
+  if i == "banana":
+    continue
+  print(i)
+
+#Else in for loop 
+
+for i in range(6):
+  print(i)
+else:
+  print("Finally finished!")
+
+#Nested Loop
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+#While Loop
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+'''
